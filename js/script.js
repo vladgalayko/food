3f0bbf568e269ff8aef141a09bab4a0083e3a607
Vlad <example@gmail.com>
2022-06-12 15:40:10 +0300

    add zero timer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 window.addEventListener('DOMContentLoaded', () => {
     // Tabs
 
@@ -44,11 +45,20 @@ window.addEventListener('DOMContentLoaded', () => {
     const deadline = '2022-06-20';
 
     function getTimeRemaining(endtime) {
-        const t = Date.parse(endtime) - Date.parse(new Date()),
-              days = Math.floor(t / (1000 * 60 * 60 * 24)),
-              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-              minutes = Math.floor((t / 1000 / 60) % 60),
-              seconds = Math.floor((t / 1000) % 60);
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date());
+
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor( (t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor( (t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        }
 
         return {
             'total': t,
